3d3a36fcdd742bfb353e0074d56c12a124486387
Louis Lab <louisliu614@gmail.com>
2022-07-12 20:35:04 +0800

    fix: fix a bug of labeltitle unworked sometimes.

--- labelimage/labeltitle.m
+++ labelimage/labeltitle.m
@@ -1,36 +1,26 @@
-function imgTitle = labeltitle(img, postInfo, title)
+function imgTitle = labeltitle(img, postInfo, titles)
+%LABELTITLE labels the image sequence with titles.
+%   labeltitle(img, postInfo, title) labels the image sequence [img] combined
+%   with multiple channels with the corresponding title in [titles],
+%   according to the info in [postInfo].  The labelled position is on the
+%   top-right corner by default. 
 
 compressedImg = postInfo.compressedSize;
 imgHeight = compressedImg(1);
 imgWidth = compressedImg(2);
 nImg = size(img, 3);
-% [imgHeight, imgWidth, nImg] = size(img);
-if nImg == 1
-    imgTitle = img;
-    return
-end
-% fps = postInfo.final_fps;
-% post_info_f = postInfo;
 imgTitle = img;
 
-% position = [imgWidth-round(0.007*imgWidth) imgHeight-round(0.007*imgHeight)];
-% bgcolor = mean2(img(position(1):position(1)+50, position(2):position(2)+50,1));
-% if bgcolor > 200
-%     textcolor = 'black';
-% else
-%     textcolor = 'white';
-% end
-
 fontsize = round(30/720*max(postInfo.compressedSize)); % default 26 pt for 720 px
-nTitle = min(length(postInfo.frames), length(title));
-if length(title) > nTitle 
-    title = title(1:nTitle);
+nTitle = min(length(postInfo.frames), length(titles));
+if length(titles) > nTitle 
+    titles = titles(1:nTitle);
 end
 textcolor = cell(nTitle, 1);
 
 position = [imgWidth*(1:nTitle)'-round(0.007*imgWidth) round(0.007*imgHeight)*ones(nTitle, 1)];
 for iTitle = 1:nTitle
-    bgcolor = mean2(img(position(iTitle,1):position(1)-50, position(iTitle,2):position(2)-50,1));
+    bgcolor = mean2(img(position(iTitle,2):position(iTitle,2)+50, (position(iTitle,1)-50):position(iTitle,1), 1));
     if bgcolor > 200
         textcolor{iTitle} = 'black';
     else
@@ -39,9 +29,8 @@ for iTitle = 1:nTitle
 end
 
 for iImg = 1:nImg
-    RGB = insertText(imgTitle(:,:,iImg),position,title,'Font','Lucida Console' ,'FontSize',fontsize,'BoxOpacity', 0, 'TextColor',textcolor, 'AnchorPoint','RightTop');
+    RGB = insertText(imgTitle(:,:,iImg),position,titles,'Font','Lucida Console' ,'FontSize',fontsize,'BoxOpacity', 0, 'TextColor',textcolor, 'AnchorPoint','RightTop');
     imgTitle(:,:,iImg) = RGB(:,:,1);
-    %     dispbar(iImg, nImg);
 end
 
 end
--- test.m
+++ test.m
@@ -2,10 +2,12 @@
 disptitle('Loading Nd2 Data')
 postInfo =  nd2analysis(filename, objective, nFreqDiv, exportPara);
 disptitle('Finish Loading')
-% Compress and contrast.
+
+% Compress and adjust contrast.
 if processPara.contrastMethod == 2
     postInfo.manualContrastPara = manualcontrastmovie(filename, postInfo.frames, postInfo.exportedChannelNo);
 end
+
 if processPara.drawROI == 1
     [postInfo.rotateAngle, postInfo.roiRect, postInfo.cropSize] = drawroiGUI(filename, postInfo, processPara.contrastMethod).getroi(); % [x y width height]
     postInfo = updateroiinfo(postInfo, exportPara.shortestSideLength);
@@ -13,8 +15,10 @@ end
 
 imgCompressed = imgcompress(filename, postInfo, processPara.contrastMethod);
 nImg = size(imgCompressed, 3);
+
 % Concatenate image stack
 imgCat = catimg(imgCompressed, postInfo);
+
 % Stamp time
 if processPara.needTimeStamp && nImg > 1
     disptitle('Stamping time')
@@ -22,6 +26,7 @@ if processPara.needTimeStamp && nImg > 1
 else
     imgTime = imgCat;
 end
+
 % Label title
 if ~isempty(processPara.title)
     disptitle('Label title')
@@ -29,19 +34,27 @@ if ~isempty(processPara.title)
 else
     imgTitle = imgTime;
 end
+
+% Extract snapshot
+if needSnapshot
+    snapshot = imgsnap(imgTime, nSnap);
+end
+
 % Label scalebar
 if processPara.needScalebar
     [imgScalebar, barInfo] = labelscale(imgTitle, postInfo);
 else
     imgScalebar = imgTime;
 end
+
 % Label text of scalebar
 if processPara.needScaleText && nImg > 1
     imgText = labelscaletext(imgScalebar, barInfo, postInfo);
 else
     imgText = imgScalebar;
 end
-%Convert img to .avi
+
+% Convert img to movie
 savename = [savedir '\' postInfo.name(1:end-4) '_scalebar' num2str(barInfo.scalebarUm) 'um'];
 if strcmp(postInfo.duration, 'N/A') || size(imgTime, 3) == 1
     imwrite(imgText, [savename '.png']);
@@ -51,23 +64,7 @@ else
     videowrite(imgText, savename, frameRate, isCompressed);
 end
 
-% Need Snapshot
-% if needSnapshot && nImg > 1
-%     disptitle('Generating the snapshots');
-%     iSnapshot = round(linspace(1,size(imgTime, 3)-10, nSnap));
-%     icurrent = 0;
-%     snapshot = cell(nSnap, 1);
-%     for i = iSnapshot
-%         icurrent = icurrent + 1;
-%         if i == iSnapshot(end)
-%             snapshot{icurrent} = labelscale(imgTime(:,:,i), postInfo);
-%         else
-%             snapshot{icurrent} = imgTime(:,:,i);
-%         end
-%     end
-%     figure
-%     montage(snapshot, 'ThumbnailSize', []);
-%     axis off;
-% 
-% saveas(gcf, [savename '.png']);
-% end
+% Snapshot labeling
+if needSnapshot
+    savesnapshot(snapshot, postInfo, processPara.title, savename);
+end
