abe9b238abd08afb2a989ea2c37083628d1ffa45
Louis Lab <louisliu614@gmail.com>
2022-07-16 16:35:55 +0800

    fix: Fix size of output montage
    Change function of saveimg without white borders to imwrite.

--- labelimage/savesnapshot.m
+++ labelimage/savesnapshot.m
@@ -1,16 +1,18 @@
 function savesnapshot(snapshot, postInfo, titleOfImg, savename)
 %SAVESNAPSHOT labels and saves the labelled snapshot.
-%   savesnapshot(snapshot, postInfo, imgTitle, savename) labels the first
+%   savesnapshot(snapshot, postInfo, titleOfImg, savename) labels the first
 %   image with titles in [titleOfImg] and the last image with scalebar
 %   according to [postInfo] and saves the labelled snapshots in the
 %   [savename].
 
 snapshot{end} = labelscale(snapshot{end}, postInfo);
 snapshot{1} = labeltitle(snapshot{1}, postInfo, titleOfImg);
-figure
-montage(snapshot, 'ThumbnailSize', [], Size=[length(snapshot) 1]);
-axis off;
-saveimg([savename, '.png']);
+fullname = [savename '.jpg'];
+fig = figure('Visible','off');
+cdata = montage(snapshot, 'ThumbnailSize', [], Size=[length(snapshot) 1]);
+axis off
+imwrite(cdata.CData, fullname);
+close(fig);
 disptitle('Successfully save the snapshot in ');
-disptitle([savename, '.png']);
+disptitle(fullname);
 end
